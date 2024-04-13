07fe2f64a33a7f3fec380e8c21daf1d166e0124e
Austin Spraggins <atmosphere9999@gmail.com>
2024-04-12 17:18:48 -0700

    Update sitemap.xml and home.js components

--- src/components/home.js
+++ src/components/home.js
@@ -6,6 +6,9 @@ import OurSpecials from "./OurSpecials";
 import SpecialEvents from "./specialEvents"; // Import SpecialEvents
 import Contact from "./contact";
 
+// This is the main component that renders all the components that the home page is composed of
+// The components are rendered in the order they appear on the page
+// I removed ourSpecials from the Home component. Add it in under ourFavoritesMenu when needed.
 const Home = () => {
   return (
     <>
