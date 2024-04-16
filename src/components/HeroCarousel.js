229a3a073e9571c2a09001e4a0b4fc03cffef532
Austin Spraggins <atmosphere9999@gmail.com>
2024-04-16 16:56:40 -0700

    updated About.css for lighthouse report, gave explicit height to logo image.

--- src/components/HeroCarousel.js
+++ src/components/HeroCarousel.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import './HeroCarousel.css';
-import heroImage from '../assets/carousel-image.jpg'; // Make sure the path is correct
+import React from "react";
+import "./HeroCarousel.css";
+import heroImage from "../assets/carousel-image.jpg"; // Make sure the path is correct
 
 const HeroCarousel = () => {
-    return (
-        <div className="heroCarousel-container">
-            <h1 className="heroCarousel-title">
-                Gelateria <span className="smaller">Del</span> CentrO
-            </h1>
-            <img src={heroImage} alt="Gelateria Del Centro" className="heroCarousel-image" />
-        </div>
-    );
+  return (
+    <div className="heroCarousel-container">
+      <h1 className="heroCarousel-title">
+        Gelateria <span className="smaller">Del</span> CentrO
+      </h1>
+      <img
+        src={heroImage}
+        alt="Gelateria Del Centro"
+        className="heroCarousel-image"
+      />
+    </div>
+  );
 };
 
 export default HeroCarousel;
